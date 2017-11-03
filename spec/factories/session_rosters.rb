e5468ed04efdc6d075d465f7ed90df2724caf54c
zhirschtirtt <zhirschtritt@gmail.com>
2017-11-03 13:19:59 -0400

    removes some old tests

--- spec/factories/session_rosters.rb
+++
@@ -1,5 +0,0 @@
-FactoryBot.define do
-  factory :session_roster do
-    
-  end
-end
--- spec/factories/sessions.rb
+++
@@ -1,8 +0,0 @@
-FactoryBot.define do
-  factory :session do
-    name "MyString"
-    description "MyString"
-    start_date "2017-11-01"
-    end_date "2017-11-01"
-  end
-end
