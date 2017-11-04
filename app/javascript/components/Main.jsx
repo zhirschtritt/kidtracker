2bc44347b17776f98876f2a070a5354968190016
zhirschtirtt <zhirschtritt@gmail.com>
2017-11-04 14:37:50 -0400

    Revert "added basic login screen"
    This reverts commit aed0a9a785daa0dbf648fef909356c3448a03208.

--- app/javascript/components/Main.jsx
+++ app/javascript/components/Main.jsx
@@ -4,7 +4,6 @@ import NavBar from './NavBar';
 import BackButton from './BackButton';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import axios from 'axios';
-import Login from './Login';
 import getMuiTheme from 'material-ui/styles/getMuiTheme';
 import darkBaseTheme from 'material-ui/styles/baseThemes/darkBaseTheme';
 
@@ -18,7 +17,6 @@ class Main extends React.Component {
       user: {},
     };
     this.loadOrganizations = this.loadOrganizations.bind(this);
-    this.loadUser = this.loadUser.bind(this);
   }
 
   loadOrganizations() {
@@ -35,23 +33,16 @@ class Main extends React.Component {
     axios.get('/api/v1/user')
     .then(response => {
       this.setState({
-        user: response.data,
-        loggedIn: true,
+        user: response.data
       });
-      this.loadOrganizations();
-    }).catch(err => this.setState({ loggedIn: false }));
+    });
   }
 
   componentDidMount() {
-    this.loadUser();
+    this.loadOrganizations();
   }
 
   render() {
-    let loadApp = <Login />;
-    if (this.state.loggedIn) {
-      loadApp = this.props.children;
-    }
-
     return(
       <MuiThemeProvider>
         <div>
@@ -60,7 +51,7 @@ class Main extends React.Component {
             organizations={this.state.organizations}
             defaultOrganization={this.state.defaultOrganization}
           />
-          {loadApp}
+          {this.props.children}
         </div>
       </MuiThemeProvider>
     );
